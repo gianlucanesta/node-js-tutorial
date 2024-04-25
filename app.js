06e482a8fa648fe4545b729b7a341bfe1789e95b
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-25 18:02:35 +0200

    Redirecting Requests

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
@@ -9,6 +10,13 @@ const server = http.createServer((req, res) => {
       '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
     res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && req.method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
     return res.end();
   }
 
