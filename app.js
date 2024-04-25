1be051123c9e1c60f6cdde14d30f06bb8abcbbe2
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-25 17:02:07 +0200

    The Node Lifecycle & Event Loop

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => console.log(req));
+const server = http.createServer((req, res) => {
+  console.log(req);
+  process.exit(0);
+});
 
 server.listen(3000);
