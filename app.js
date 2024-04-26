b07eaaeac7444cd47ff9ba1c155abe71efd42a91
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-26 19:40:27 +0200

    Understanding NPM Scripts

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const http = require("http");
 
 const routes = require("./routes");
 
-console.log(routes.someText);
+// console.log(routes.someText);
 
 const server = http.createServer(routes.handler);
-server.listen(3000);
+server.listen(3000, () => {
+  console.log("Server is listening on port 3000...");
+});
