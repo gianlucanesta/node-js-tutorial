cf07035e6c4d7933b4b7081dd7c364e5a68cfdcb
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-27 19:58:52 +0200

    Sharing Data Across Requests & Users

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,20 @@
 const path = require("path");
 const express = require("express");
-
+const router = express.Router();
 const rootDir = require("../util/path");
 
-const router = express.Router();
+const products = [];
 
-router.use("/add-product", (req, res, next) => {
+router.get("/add-product", (req, res, next) => {
   res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
-router.post("/product", (req, res, next) => {
-  console.log(req.body);
+router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
+  // console.log(req.body);
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -4,9 +4,12 @@ const express = require("express");
 
 const rootDir = require("../util/path");
 
+const adminData = require("./admin");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+  console.log("In shop.js", adminData.products);
   // res.sendFile("/views/shop.html", { root: __dirname + "../../" });
   // res.sendFile("/views/shop.html", { root: path.join(__dirname, "..") });
   res.sendFile(path.join(rootDir, "views", "shop.html"));
