fb6a2c7e23304e86697cf9ffed0e56c75305b352
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-27 16:20:02 +0200

    Adding Middleware

--- app.js
+++ app.js
@@ -4,9 +4,18 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("In another middleware");
+});
+
 // console.log(routes.someText);
 
-const server = http.createServer(routes.handler);
+const server = http.createServer(app);
 server.listen(3000, () => {
   console.log("Server is listening on port 3000...");
 });
