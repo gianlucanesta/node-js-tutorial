b1b5c14668baa1dd3427b59360f6da19b5e0c857
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-27 16:55:08 +0200

    Parsing Incoming Requests

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use("/add-product", (req, res, next) => {
-  res.send("<h1>Products!</h1>");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
   res.send("<h1>Hello from Express!</h1>");
 });
 
-app.listen(3000);
+app.listen(PORT, () => {
+  console.log("Server started on port 3000");
+});
