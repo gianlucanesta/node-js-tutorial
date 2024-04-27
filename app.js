df4578f675da9d63a793075ec6b8670929bc093c
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-27 18:51:52 +0200

    Serving Files Statically

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const PORT = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
