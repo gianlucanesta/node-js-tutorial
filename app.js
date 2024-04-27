43d0c5a4ed7a416edb70a6a516043407b3de3f0e
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-27 16:26:48 +0200

    Express.js - Looking Behind the Scenes

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
@@ -16,7 +14,4 @@ app.use((req, res, next) => {
 
 // console.log(routes.someText);
 
-const server = http.createServer(app);
-server.listen(3000, () => {
-  console.log("Server is listening on port 3000...");
-});
+app.listen(3000);
