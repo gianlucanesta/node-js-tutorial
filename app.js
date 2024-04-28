66fbdfb7c1fb42331a312e03e36e439ee0c35ae7
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-28 18:06:32 +0200

    Preparations

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const errorController = require("./controllers/error");
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const PORT = 3000;
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(PORT, () => {
-  console.log("Server started on port", PORT);
-});
+app.listen(3000);
