f9037064052f0ffd8795de6a7fe4d5483396e8d5
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-28 12:29:38 +0200

    Finishing the Pug Template

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,7 @@ const products = [];
 router.get("/add-product", (req, res, next) => {
   res.render("add-product", {
     pageTitle: "Add Product",
+    path: "/admin/add-product",
   });
 });
 
--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,8 @@ router.get("/", (req, res, next) => {
   const products = adminData.products;
   res.render("shop", {
     prods: products,
-    docTitle: "Shop",
+    pageTitle: "Shop",
+    path: "/shop",
   });
 });
 
