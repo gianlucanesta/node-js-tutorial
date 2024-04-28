2869daee9f785ca15ee554076e872bf662a1a9c8
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-28 17:57:12 +0200

    Editing & Deleting Products

--- models/product.js
+++ models/product.js
@@ -1,18 +1,19 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const p = path.join(
-  path.dirname(require.main.filename),
-  "data",
-  "products.json"
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
 );
 
-const getProductsFromFile = (cb) => {
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
-      return cb([]);
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-    cb(JSON.parse(fileContent));
   });
 };
 
@@ -25,10 +26,9 @@ module.exports = class Product {
   }
 
   save() {
-    getProductsFromFile((products) => {
+    getProductsFromFile(products => {
       products.push(this);
-
-      fs.writeFile(p, JSON.stringify(products), (err) => {
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
     });
--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,17 @@
-const express = require("express");
-const shopController = require("../controllers/shop");
+const path = require('path');
+
+const express = require('express');
+
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get("/", shopController.getIndex);
+router.get('/', shopController.getIndex);
 
-router.get("products", shopController.getProducts);
+router.get('/products', shopController.getProducts);
 
-router.get("/cart", shopController.getCart);
+router.get('/cart', shopController.getCart);
 
-router.get("/checkout", shopController.getCheckout);
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
