fd9b9fa3344465825a380b46c4f0577faffa6efb
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-28 15:50:03 +0200

    Finishing the Controllers

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const errorController = require("./controllers/error");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const app = express();
@@ -18,10 +19,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+app.use(errorController.get404);
 
 app.listen(PORT, () => {
-  console.log("Server started on port 3000");
+  console.log("Server started on port", PORT);
 });
