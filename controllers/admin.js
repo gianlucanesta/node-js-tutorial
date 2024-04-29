d8fb46fceb6cd213a2d5894345a75ba1f1728a59
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-29 21:01:37 +0200

    Linking to the Edit Page

--- controllers/admin.js
+++ controllers/admin.js
@@ -38,6 +38,14 @@ exports.getEditProduct = (req, res, next) => {
   });
 };
 
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+};
+
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("admin/products", {
