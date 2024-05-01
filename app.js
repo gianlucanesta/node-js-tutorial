3022e4fcec4187a1b3cfdebb9baeb914ec1b5434
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-05-01 16:12:35 +0200

    Retrieving Data & Finding Products

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 sequelize
-  .sync({ force: true })
+  .sync()
   .then((result) => {
-    console.log(result);
+    // console.log(result);
     app.listen(3000);
   })
   .catch((err) => {
