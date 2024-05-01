e80db244c59bea6937c0e89284c0a2af0e57f4f5
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-05-01 18:06:44 +0200

    Creating & Managing a Dummy User

--- app.js
+++ app.js
@@ -21,6 +21,15 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
@@ -30,10 +39,19 @@ app.use(errorController.get404);
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 
 sequelize
-  .sync({ force: true })
+  .sync()
   .then((result) => {
-    // console.log(result);
-    app.listen(3000);
+    User.findByPk(1)
+      .then((user) => {
+        if (!user) {
+          return User.create({ name: "Max", email: "test@test.com" });
+        }
+        return user;
+      })
+      .then((user) => {
+        console.log(user);
+        app.listen(3000);
+      });
   })
   .catch((err) => {
     console.log(err);
