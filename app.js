050d1447529526d99dfbabab516c30f130a5f69a
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-05-06 21:31:45 +0200

    Installing the MongoDB Driver

--- app.js
+++ app.js
@@ -4,88 +4,41 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
+const mongoConnect = require("./util/database");
 
-const sequelize = require("./util/database");
-
-const Product = require("./models/product");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
+// const Product = require("./models/product");
+// const User = require("./models/user");
+// const Cart = require("./models/cart");
+// const CartItem = require("./models/cart-item");
+// const Order = require("./models/order");
+// const OrderItem = require("./models/order-item");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+// const adminRoutes = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => console.log(err));
+  // User.findByPk(1)
+  //   .then((user) => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch((err) => console.log(err));
 });
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// Associating a product to a user
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-
-// Associating a user to a cart
-User.hasMany(Product);
-
-// Associating a user to a cart
-User.hasOne(Cart);
-
-// Associating a cart to a user
-Cart.belongsTo(User);
-
-// Associating a cart to a product
-Cart.belongsToMany(Product, { through: CartItem });
-
-// Associating a product to a cart
-Product.belongsToMany(Cart, { through: CartItem });
-
-// Associating an order to a user
-Order.belongsTo(User);
-
-// Associating a user to an order
-User.hasMany(Order);
-
-// Associating an order to a product
-Order.belongsToMany(Product, { through: OrderItem });
-
-sequelize
-  .sync()
-  // .sync({ force: true })
-  .then((result) => {
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: "Max", email: "test@test.com" });
-    }
-    return user;
-  })
-  .then((user) => {
-    // console.log(user);
-    return user.createCart();
-  })
-  .then((cart) => {
-    // console.log(cart);
-    app.listen(3000);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoConnect((client) => {
+  console.log(client);
+  app.listen(3000);
+});
--- util/database.js
+++ util/database.js
@@ -1,13 +1,42 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
 
-const sequelize = new Sequelize(
-  "node-complete",
-  "root",
-  "ubiJT;YPsr3[kosws{S;p7-X;-9;xJ",
-  {
-    dialect: "mysql",
-    host: "localhost",
-  }
-);
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  return MongoClient.connect(
+    "mongodb+srv://gianlucanesta:JqhYo1lxsh4JlbZI@cluster0.h68rwui.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+    .then((client) => {
+      console.log("Connected");
+      callback(client);
+    })
+    .catch((err) => console.log(err));
+};
+
+module.exports = mongoConnect;
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://gianlucanesta:JqhYo1lxsh4JlbZI@cluster0.h68rwui.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
