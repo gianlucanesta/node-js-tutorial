ca0ec806eae52cddad9525cc639e4d7239c49916
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-05-07 23:01:40 +0200

    Fixing the "Add Product" Functionality

--- models/product.js
+++ models/product.js
@@ -7,7 +7,7 @@ class Product {
     this.imageUrl = imageUrl;
     this.description = description;
     this.price = price;
-    this._id = new mongodb.ObjectId(id);
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
 
   save() {
