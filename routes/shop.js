c4a18fba089215137c6d36473c974d50c2931aeb
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-05-14 20:10:44 +0200

    Fetching All Products

--- routes/shop.js
+++ routes/shop.js
@@ -6,9 +6,11 @@ const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
-// router.get("/", shopController.getIndex);
+// /shop/get-index => GET
+router.get("/", shopController.getIndex);
 
-// router.get("/products", shopController.getProducts);
+// /shop/get-products => GET
+router.get("/products", shopController.getProducts);
 
 // router.get("/products/:productId", shopController.getProduct);
 
