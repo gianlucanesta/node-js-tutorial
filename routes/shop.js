4e093d40340783449c8f391c9c8de6182c662147
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-05-15 22:57:49 +0200

    Loading the Cart

--- routes/shop.js
+++ routes/shop.js
@@ -15,7 +15,7 @@ router.get("/products", shopController.getProducts);
 // /shop/get-product => GET
 router.get("/products/:productId", shopController.getProduct);
 
-// router.get("/cart", shopController.getCart);
+router.get("/cart", shopController.getCart);
 
 // router.post("/cart-delete-item", shopController.postCartDeleteProduct);
 
