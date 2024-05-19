0bee75e1908a67fe3916119afe35cfd9af54f1d0
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-05-19 19:26:15 +0200

    Providing User Feedback

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const mongoDbStore = require("connect-mongodb-session")(session);
 const csrf = require("csurf");
+const flash = require("connect-flash");
 
 const errorController = require("./controllers/error");
 
@@ -43,6 +44,7 @@ app.use(
 );
 
 app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
   if (!req.session.user) {
