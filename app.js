cb40a353cc081e245ba6802fabe32e810c4f081f
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-05-19 15:48:21 +0200

    Making "Add to Cart" Work Again

--- app.js
+++ app.js
@@ -41,8 +41,16 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  next();
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      req.user = user;
+      res.locals.isAuthenticated = req.session.isLoggedIn;
+      next();
+    })
+    .catch((err) => console.log(err));
 });
 
 app.use("/admin", adminRoutes);
