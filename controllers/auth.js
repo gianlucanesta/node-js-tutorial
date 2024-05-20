13c1b5dc223d6da3da61f2399476e8a702599622
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-05-20 22:39:44 +0200

    Creating the Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -136,38 +136,36 @@ exports.getReset = (req, res, next) => {
 };
 
 exports.postReset = (req, res, next) => {
-  crypto
-    .randomBytes(32, (err, buffer) => {
-      if (err) {
-        console.log(err);
-        return res.redirect("/reset");
-      }
-      const token = buffer.toString("hex");
-      User.findOne({ email: req.body.email })
-        .then((user) => {
-          if (!user) {
-            req.flash("error", "No account with that email found.");
-            return res.redirect("/reset");
-          }
-          user.resetToken = token;
-          user.resetTokenExpiration = Date.now() + 3600000;
-          return user.save();
-        })
-        .then((result) => {
-          res.redirect("/");
-          const msg = {
-            to: req.body.email,
-            from: process.env.SENDGRID_SENDER,
-            subject: "Password Reset",
-            text: "You requested a password reset",
-            html: `
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect("/reset");
+    }
+    const token = buffer.toString("hex");
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          req.flash("error", "No account with that email found.");
+          return res.redirect("/reset");
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then((result) => {
+        res.redirect("/");
+        const msg = {
+          to: req.body.email,
+          from: process.env.SENDGRID_SENDER,
+          subject: "Password Reset",
+          text: "You requested a password reset",
+          html: `
               <p>You requested a password reset</p>
               <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
             `,
-          };
-          return sgMail.send(msg);
-        })
-        .catch((err) => console.log(err));
-    })
-    .catch((err) => console.log(err));
+        };
+        return sgMail.send(msg);
+      })
+      .catch((err) => console.log(err));
+  });
 };
