bdc2e083b419af2656cd6c2cdbd3d5da4a97c138
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-05-22 19:37:33 +0200

    Adding Validation to Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,6 +20,11 @@ exports.getLogin = (req, res, next) => {
     path: "/login",
     pageTitle: "Login",
     errorMessage: message,
+    oldInput: {
+      email: "",
+      password: "",
+    },
+    validationErrors: [],
   });
 };
 
@@ -53,14 +58,29 @@ exports.postLogin = (req, res, next) => {
       path: "/login",
       pageTitle: "Login",
       errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password,
+      },
+      validationErrors: errors.array(),
     });
   }
 
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
-        req.flash("error", "Invalid email or password");
-        return res.redirect("/login");
+        return res.status(422).render("auth/login", {
+          path: "/login",
+          pageTitle: "Login",
+          errorMessage: "Invalid email or password.",
+          oldInput: {
+            email: email,
+            password: password,
+          },
+          validationErrors: [
+            { path: "email", msg: "Invalid email or password." },
+          ],
+        });
       }
       bcrypt
         .compare(password, user.password)
