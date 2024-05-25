be1bb74d4ed87774afa17b8b99a0366aacf69b1e
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-05-25 18:46:59 +0200

    Sending & Handling Background Requests

--- controllers/admin.js
+++ controllers/admin.js
@@ -173,8 +173,8 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then((product) => {
       if (!product) {
@@ -187,11 +187,9 @@ exports.postDeleteProduct = (req, res, next) => {
 
     .then((result) => {
       console.log("Prodotto eliminato");
-      res.redirect("/admin/products");
+      res.status(200).json({ message: "Product deleted." });
     })
     .catch((err) => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({ message: "Deleting product failed." });
     });
 };
--- public/js/admin.js
+++ public/js/admin.js
@@ -1,4 +1,22 @@
-const deleteProduct = (productId) => {
-  const csrfToken = document.querySelector('input[name="_csrf"]').value;
-  const productElement = document.getElementById(productId);
+const deleteProduct = (btn) => {
+  const prodId = btn.parentNode.querySelector("[name=productId]").value;
+  const csrfToken = btn.parentNode.querySelector("[name=_csrf]").value;
 
+  const productElement = btn.closest("article");
+  fetch("/admin/product/" + prodId, {
+    method: "DELETE",
+    headers: {
+      "csrf-token": csrfToken,
+    },
+  })
+    .then((result) => {
+      return result.json();
+    })
+    .then((data) => {
+      // console.log("data", data);
+      productElement.parentNode.removeChild(productElement);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
