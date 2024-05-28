b536ee564194388424bca1894bf95cf6e1278d14
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-05-28 23:15:41 +0200

    Logging In & Creating JSON Web Tokens (JWTs)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
-const User = require("../models/user"); // Assicurati che il percorso sia corretto
+const User = require("../models/user"); 
+const jwt = require("jsonwebtoken");
 
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
