696813342ac722ac28d517113ae32ad2576c71fc
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-06-02 12:44:50 +0200

    Handling Errors

--- app.js
+++ app.js
@@ -69,6 +69,15 @@ app.use(
     schema: graphqlSchema,
     rootValue: graphqlResolver,
     context: (req) => req.headers,
+    formatError: (error) => {
+      if (!error.originalError) {
+        return error;
+      }
+      const data = error.originalError.data;
+      const message = error.message || "An error occurred";
+      const code = error.originalError.code || 500;
+      return { message: message, status: code, data: data };
+    },
   })
 );
 app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
