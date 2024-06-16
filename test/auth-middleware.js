40f77ce1c3be8205f31a7a15f9526d7d1d2808d3
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-06-16 15:07:15 +0200

    Using Stubs

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -2,6 +2,8 @@ async function getExpect() {
   const expect = await import("chai").then((mod) => mod.expect);
   return expect;
 }
+const sinon = require("sinon");
+const jwt = require("jsonwebtoken");
 
 const authMiddleware = require("../middleware/is-auth");
 
@@ -47,8 +49,18 @@ describe("Auth middleware", function () {
         return "Bearer akjhkjhakjh";
       },
     };
-    authMiddleware.bind(this, req, {}, () => {});
+    sinon.stub(jwt, "verify");
+    jwt.verify.returns({
+      userId: "abc",
+    });
+    // jwt.verify = function () {
+    //   return { userId: "abc" };
+    // };
+    authMiddleware(req, {}, () => {});
 
     expect(req).to.have.property("userId");
+    expect(req).to.have.property("userId", "abc");
+    expect(jwt.verify.called).to.be.true;
+    jwt.verify.restore();
   });
 });
