4598fecfaedf42dbf432867f7a3db1754fb44559
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-06-16 14:25:02 +0200

    Organizing Multiple Tests

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -5,15 +5,28 @@ async function getExpect() {
 
 const authMiddleware = require("../middleware/is-auth");
 
-it("should throw an error if no authorization header is present", async function () {
-  const expect = await getExpect();
+describe("Auth middleware", function () {
+  it("should throw an error if no authorization header is present", async function () {
+    const expect = await getExpect();
 
-  const req = {
-    get: function (headerName) {
-      return null;
-    },
-  };
-  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
-    "Not authenticated."
-  );
+    const req = {
+      get: function (headerName) {
+        return null;
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      "Not authenticated."
+    );
+  });
+
+  it("should throw an error if the authorization header is only one string", async function () {
+    const expect = await getExpect();
+    const req = {
+      get: function (headerName) {
+        return "abc";
+      },
+    };
+
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
 });
