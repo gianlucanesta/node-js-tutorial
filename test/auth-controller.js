466968439d4b9bb058ba07333508d2ae8b8fcfc1
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-06-16 17:36:18 +0200

    Hooks

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -13,31 +13,8 @@ const User = require("../models/user");
 
 const AuthController = require("../controllers/auth");
 
-describe("Auth controller - LOGIN", function () {
-  it("should throw an error if accessing the database fails", async function () {
-    sinon.stub(User, "findOne");
-    User.findOne.throws();
-
-    const req = {
-      body: {
-        email: "a@a.com",
-        password: "1234",
-      },
-    };
-
-    AuthController.login(req, {}, () => {}).then((result) => {
-      // console.log("result:", result);
-      const expect = getExpect();
-
-      expect(result).to.be.an("error");
-      expect(result).to.have.property("statusCode", 500);
-      // done()
-    });
-
-    User.findOne.restore();
-  });
-
-  it("should send a response with a valid user status for an existing user", async function (done) {
+describe("Auth controller", function () {
+  before(function (done) {
     mongoose
       .connect(process.env.MONGODB_URI_TEST)
       .then((result) => {
@@ -50,34 +27,65 @@ describe("Auth controller - LOGIN", function () {
         });
         return user.save();
       })
-      .then(() => {
-        const req = {
-          userId: "5f0f5e5b9c7b0b0b0b0b0b0b",
-        };
-        const res = {
-          statusCode: 500,
-          userStatus: null,
-          status: function (code) {
-            this.statusCode = code;
-            return this;
-          },
-          json: function (data) {
-            this.userStatus = data.status;
-          },
-        };
+      .then((result) => {
+        done();
+      });
+  });
 
-        AuthController.getUserStatus(req, res, () => {}).then(() => {
-          const expect = getExpect();
-          expect(res.statusCode).to.be.equal(200);
-          expect(res.userStatus).to.be.equal("I am new!");
-          User.deleteMany({});
-          mongoose.connection.close().then(() => {
-            done();
-          });
-        });
+  beforeEach(function () {});
+
+  afterEach(function () {});
+
+  it("should throw an error if accessing the database fails", async function () {
+    sinon.stub(User, "findOne");
+    User.findOne.throws();
+
+    const req = {
+      body: {
+        email: "a@a.com",
+        password: "1234",
+      },
+    };
+
+    const result = await AuthController.login(req, {}, () => {});
+
+    const expect = await getExpect();
+    expect(result).to.be.an("error");
+    expect(result).to.have.property("statusCode", 500);
+
+    User.findOne.restore();
+  });
+
+  it("should send a response with a valid user status for an existing user", async function () {
+    const req = {
+      userId: "5f0f5e5b9c7b0b0b0b0b0b0b",
+    };
+    const res = {
+      statusCode: 500,
+      userStatus: null,
+      status: function (code) {
+        this.statusCode = code;
+        return this;
+      },
+      json: function (data) {
+        this.userStatus = data.status;
+      },
+    };
+
+    await AuthController.getUserStatus(req, res);
+
+    const expect = await getExpect();
+    expect(res.statusCode).to.be.equal(200);
+    expect(res.userStatus).to.be.equal("I am new!");
+  });
+
+  after(function (done) {
+    User.deleteMany({})
+      .then(() => {
+        return mongoose.disconnect();
       })
-      .catch((err) => {
-        console.log(err);
+      .then(() => {
+        done();
       });
   });
 });
