1f621b5d218416163afb36289698f159bb3bf4ac
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-06-16 16:43:05 +0200

    Testing Code With An Active Database

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -38,15 +38,43 @@ describe("Auth controller - LOGIN", function () {
   });
 
   it("should send a response with a valid user status for an existing user", async function (done) {
-    mongoose.connect(process.env.MONGODB_URI_TEST).then((result) => {
-      const user = new User({
-        email: "a@a.com",
-        password: "1234",
-        name: "name",
-        posts: [],
-      }).catch((err) => {
+    mongoose
+      .connect(process.env.MONGODB_URI_TEST)
+      .then((result) => {
+        const user = new User({
+          email: "a@a.com",
+          password: "1234",
+          name: "name",
+          posts: [],
+          _id: "5f0f5e5b9c7b0b0b0b0b0b0b",
+        });
+        return user.save();
+      })
+      .then(() => {
+        const req = {
+          userId: "5f0f5e5b9c7b0b0b0b0b0b0b",
+        };
+        const res = {
+          statusCode: 500,
+          userStatus: null,
+          status: function (code) {
+            this.statusCode = code;
+            return this;
+          },
+          json: function (data) {
+            this.userStatus = data.status;
+          },
+        };
+
+        AuthController.getStatus(req, res, () => {}).then(() => {
+          const expect = getExpect();
+          expect(res.statusCode).to.be.equal(200);
+          expect(res.userStatus).to.be.equal("I am new!");
+          done();
+        });
+      })
+      .catch((err) => {
         console.log(err);
       });
-    });
   });
 });
