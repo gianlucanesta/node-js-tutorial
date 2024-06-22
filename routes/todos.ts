b5a8f27a99057ddf335de9ec695369c1e3ebf7df
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-06-22 12:44:58 +0200

    Finishing the REST Routes

--- routes/todos.ts
+++ routes/todos.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 import { Todo } from "../model/todo";
 
-const todos: Todo[] = [];
+let todos: Todo[] = [];
 
 const router = Router();
 
@@ -17,6 +17,26 @@ router.post("/todo", (req, res, next) => {
   };
 
   todos.push(newTodo);
+  res.status(201).json({ message: "Todo added", todo: newTodo, todos: todos });
+});
+
+router.put("/todo/:todoId", (req, res, next) => {
+  const todoId = req.params.todoId;
+  const updatedTitle = req.body.title;
+
+  const todoIndex = todos.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex >= 0) {
+    todos[todoIndex] = { id: todos[todoIndex].id, title: updatedTitle };
+    res.status(200).json({ message: "Todo updated", todos: todos });
+  }
+  res.status(404).json({ message: "Could not find todo for this id." });
+});
+
+router.delete("/todo/:todoId", (req, res, next) => {
+  const todoId = req.params.todoId;
+  todos = todos.filter((todo) => todo.id !== todoId);
+  res.status(200).json({ message: "Todo deleted", todos: todos });
 });
 
 export default router;
