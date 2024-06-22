307ffb47ec6c5aef523d2784e5023dca20871607
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-06-22 17:52:17 +0200

    Creating a Webserver

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const fs = require("fs").promises;
+const http = require("http");
 
-const text = "This is a test - and it should be stored in a file!";
+const server = http.createServer((req, res) => {
+  res.end("Hello, World!");
+});
 
-fs.writeFile("node-message.txt", text).then(() =>
-  console.log("Successfully wrote to file")
-);
+server.listen(3000);
