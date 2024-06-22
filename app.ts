64f4c5bbd5d8a993bc7f2937e9bd53ab1eadd1c4
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-06-22 17:49:43 +0200

    Using the Standard Library

--- app.ts
+++ app.ts
@@ -1,8 +1,4 @@
-const text = "This is a test - and it should be stored in a file!";
-
-const encoder = new TextEncoder();
-const data = encoder.encode(text);
-
-Deno.writeFile("test.txt", data).then(() =>
-  console.log("Successfully wrote to file")
-);
+function handler(_req: Request): Response {
+  return new Response("Hello, World!");
+}
+Deno.serve(handler);
