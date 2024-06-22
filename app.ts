8045eda0c502c016c706217c9e1ac7c4fd13cb16
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-06-22 16:45:03 +0200

    Using the Runtime API

--- app.ts
+++ app.ts
@@ -1,4 +1,8 @@
-let message: string;
-message = "Hi there";
+const text = "This is a test - and it should be stored in a file!";
 
-console.log(message);
+const encoder = new TextEncoder();
+const data = encoder.encode(text);
+
+Deno.writeFile("test.txt", data).then(() =>
+  console.log("Successfully wrote to file")
+);
