9723e0263d072997cc5acd43507ce52ad4091bdd
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-06-22 18:45:08 +0200

    Using the Oak Framework with Deno

--- app.ts
+++ app.ts
@@ -1,4 +1,14 @@
-function handler(_req: Request): Response {
-  return new Response("Hello, World!");
-}
-Deno.serve(handler);
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello, World!";
+});
+
+await app.listen({ port: 8000 });
+
+// function handler(_req: Request): Response {
+//   return new Response("Hello, World!");
+// }
+// Deno.serve(handler);
